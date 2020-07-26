9c6ebe176d4358ee5b309ce0a0cc94818957d941
MarcoMiretti <marco.miretti@gmail.com>
2020-07-26 15:53:28 -0300

    :hammer: Refactor as octave package

---
+++ inst/ws_connect.m
@@ -0,0 +1,30 @@
+## Copyright (C) 2020 Marco Miretti <marco.miretti@gmail.com>
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 3 of the
+## License, or (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {@var{status} =} ws_connect (@var{uri})
+## Establishes a websocket connection with the specified uri.
+##
+## @var{uri}: a string with the form of "ws://host:port/path" 
+## @var{status}: connection status code.
+##
+## @end deftypefn
+
+function ret = ws_connect (uri)
+
+  disp ("not yet implemented")
+  ret = -1;
+
+endfunction
